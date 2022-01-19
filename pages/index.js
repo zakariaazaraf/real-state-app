b4ba7c2f5f11e47283f9cf2830175bec1a0b29fb
zakaria <zakariaazaraf@gmail.com>
2022-01-19 22:48:59 +0100

    Added The property component to display the house with the details

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,8 @@ import { Box, Flex, Text, Button } from '@chakra-ui/react'
 
 import { urlBase, fetchApiData } from '../utils/FetchAPI'
 
+import Property from '../components/Property'
+
 const Banner = ({ imageUrl, purpose, title1, title2, desc1, desc2, linkName, buttonText }) => {
   return <>
     <Flex flexWrap='wrap' justifyContent='center' alignItems='center' m='10'>
@@ -35,7 +37,14 @@ const Home = ( {propertiesForSale, propertiesForRent} ) => {
             linkName='/search?purpose=for-rent'
             imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4'
           />
-
+          <Flex flexWrap='wrap'>
+            {/* List the Hoses for rent here */}
+            {
+              propertiesForRent.map(property => {
+                return <Property property={property} key={property.id}/>
+              })
+            }
+          </Flex>
           <Banner
             purpose='BUY A HOME'
             title1=' Find, Buy & Own Your'
@@ -46,6 +55,14 @@ const Home = ( {propertiesForSale, propertiesForRent} ) => {
             linkName='/search?purpose=for-sale'
             imageUrl='https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008'
           />
+          <Flex flexWrap='wrap'>
+            {/* List the Hoses for sale here */}
+            {
+              propertiesForSale.map(property => {
+                return <Property property={property} key={property.id}/>
+              })
+            }
+          </Flex>
     </Box>
   </>
 }
